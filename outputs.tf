28aceeb95bdcad2a3e0278df5bf96ac0ca8ed479
João Pedro <joao_loureiro@outlook.com>
2023-04-11 16:41:45 -0300

    generate kind outputs only when kind cluster is created

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,31 @@
 #################### Kind | Local k8s cluster ####################
 
 output "kind_kubeconfig" {
-  value = kind_cluster.big-data-on-k8s-blueprint[0].kubeconfig
+  value = var.kind ? kind_cluster.big-data-on-k8s-blueprint[0].kubeconfig : null
   description = "The kubeconfig for the kind cluster after it is created"
   sensitive = true
 }
 
 output "kind_client_certificate" {
-  value = kind_cluster.big-data-on-k8s-blueprint[0].client_certificate
+  value = var.kind ? kind_cluster.big-data-on-k8s-blueprint[0].client_certificate : null
   description = "Client certificate for authenticating to kind cluster"
   sensitive = true
 }
 
 output "kind_client_key" {
-  value = kind_cluster.big-data-on-k8s-blueprint[0].client_key
+  value = var.kind ? kind_cluster.big-data-on-k8s-blueprint[0].client_key : null
   description = "Client key for authenticating to kind cluster."
   sensitive = true
 }
 
 output "kind_cluster_ca_certificate" {
-  value = kind_cluster.big-data-on-k8s-blueprint[0].cluster_ca_certificate
+  value = var.kind ? kind_cluster.big-data-on-k8s-blueprint[0].cluster_ca_certificate : null
   description = "Client verifies the server certificate with this CA cert"
   sensitive = true
 }
 
 output "kind_endpoint" {
-  value = kind_cluster.big-data-on-k8s-blueprint[0].endpoint
+  value = var.kind ? kind_cluster.big-data-on-k8s-blueprint[0].endpoint : null
   description = "Kind APIServer endpoint"
   sensitive = false
 }
